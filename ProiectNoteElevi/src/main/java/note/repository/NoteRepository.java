14e7f30765607a476753b7e2266031238b948864
Sergiu <icadan30@yahoo.com>
2018-03-19 11:46:21 +0200

    Initial commit

---
+++ ProiectNoteElevi/src/main/java/note/repository/NoteRepository.java
@@ -0,0 +1,15 @@
+package note.repository;
+
+import java.util.List;
+
+
+import note.model.Nota;
+import note.utils.ClasaException;
+
+public interface NoteRepository {
+	
+	public void addNota(Nota nota) throws ClasaException;
+	public List<Nota> getNote(); 
+	public void readNote(String fisier);
+	
+}
---
+++ ProiectNoteElevi/src/main/java/note/utils/ClasaException.java
@@ -0,0 +1,8 @@
+package note.utils;
+
+public class ClasaException extends Exception {
+
+    public ClasaException(){super();}
+    public ClasaException(String message){super(message);}
+
+}
